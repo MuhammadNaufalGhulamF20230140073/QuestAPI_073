7d11204559582867dfd41f27c4ff7c4769700a65
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-12-19 07:13:36 +0700

    Menambahkan model DataSiswa dan UIStateSiswa

---
+++ app/src/main/java/com/example/questapi/modeldata/DataSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.questapi.modeldata
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telepon : String
+)
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
