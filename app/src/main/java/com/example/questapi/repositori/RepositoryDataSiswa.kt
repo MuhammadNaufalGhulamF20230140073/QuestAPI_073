0470d6fb959e0e25585cb5d769582556580b36f2
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2026-01-02 20:02:10 +0700

    nambah fungsi edit hapus sama dapat

--- app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -2,17 +2,20 @@ package com.example.questapi.repositori
 
 import com.example.questapi.apiservice.ServiceApiSiswa
 import com.example.questapi.modeldata.DataSiswa
+import retrofit2.Response
 
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id:Int) : DataSiswa
+    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa):retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(
     private val serviceApiSiswa: ServiceApiSiswa
 ): RepositoryDataSiswa{
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa
-        .getSiswa()
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa):retrofit2
     .Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-}
+
